9ebae2bdb4666efe4e8a6bd3d86cc30cf51a11fd
Bryan Arendt <barendt@covermymeds.com>
2019-01-23 19:38:04 -0500

    add gcp

---
+++ chromewd.fish
@@ -0,0 +1,6 @@
+function chromewd
+  set -x DRIVER chrome
+  set -x WEBPACK true
+  set -x VUEJS_DEV_TOOLS '/Users/barendt/Library/Application Support/Google/Chrome/Default/Extensions/ljjemllljcmogpfapbkkighbhhppjdbg/5.0.0.3_0'
+  set -x CHROME_BINARY '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+end
